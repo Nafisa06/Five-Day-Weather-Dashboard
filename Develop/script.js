f4176569bd46914c8d8f219fc8d9befac244e2b9
Nafisa06 <nafisa.ila@gmail.com>
2022-02-01 02:01:48 +1100

    added code to select the correct days in the forecast cards

--- Develop/script.js
+++ Develop/script.js
@@ -22,8 +22,30 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appi
     }
    
    
-    //Getting Weather Icons
+//Getting Weather Icons
+
      for(i = 0; i<5; i++){
         document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+ data.list[i].weather[0].icon +".png";
     }
         console.log(data)
+
+    })
+
+// displaying text for the upcoming five days of the week
+
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+//Function to get the correct integer for the index of the days array
+function CheckDay(day){
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
+    }
+    else{
+        return day + d.getDay();
+    }
+}
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }
